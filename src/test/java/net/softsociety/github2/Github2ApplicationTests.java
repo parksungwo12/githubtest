7e8383ce979c72be8cf7170e47d40044700dc5c8
박성우\user <tjddn7500@gmail.com>
2023-03-09 10:16:33 +0900

    커밋1

---
+++ src/test/java/net/softsociety/github2/Github2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.softsociety.github2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Github2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
